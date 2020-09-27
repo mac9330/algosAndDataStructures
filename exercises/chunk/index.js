53d47b972ce50f615ba161e39e593394f03098d1
Mac9330 <macyoung94@gmail.com>
2020-09-27 17:32:47 -0400

    complete chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,23 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const matrix = [];
+    let inner = [];
+    array.forEach((el, idx) => {
+        if ((idx + 1) % size === 0) {
+            debugger
+            inner.push(el);
+            matrix.push(inner);
+            inner = [];
+        }
+        else {
+            inner.push(el);
+        }
+    });
+    inner.length ? matrix.push(inner) : null;
+    return matrix;
+}
+
 
 module.exports = chunk;
