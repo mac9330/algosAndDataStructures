afeeb6df1713cdddbfc1b7eeb1d780735431a178
Mac9330 <macyoung94@gmail.com>
2020-11-06 20:26:59 -0500

    add binary search tree

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -16,18 +16,60 @@ class Node {
         this.data = data;
         this.left = null;
         this.right = null;
+        this.insert = this.insert.bind(this);
     }
-
-    insert(data) {
-        let node = this;
-        while (node) {
-            if (data < this.data) {
-                node = this.left;
-            } else if (data > this.data) {
-                node = this.right;
+    // recursive solution
+    insert(data, node=this) {
+        //base case
+        if (data < node.data) {
+            if (node.left) {
+                this.insert(data, node.left)
+            } else {
+                return node.left = new Node(data);
             }
-
+        } else if (data > node.data) {
+            if (node.right) {
+                this.insert(data, node.right)
+            } else {
+                return node.right = new Node(data);
+            }
         }
+    }
+    // iterative solution
+    // insert(data) {
+    //     let node = this;
+    //     while (node) {
+    //         if (data < node.data) {
+    //             // node.left ? node = node.left : node.left = new Node(data); node = false;
+    //             if (node.left) {
+    //                 node = node.left; 
+    //             } else {
+    //                 node.left = new Node(data)
+    //                 node = false;
+    //             }
+    //         } else if (data > node.data) {
+    //             if (node.right) {
+    //                 node = node.right;
+    //             } else {
+    //                 node.right = new Node(data); 
+    //                 node = false
+    //             }
+    //         }
+    //     }
+    // }
+
+    contains(data) {
+        let node = this;
+        while(node) {
+            if (data < node.data) {
+                node = node.left;
+            } else if (data > node.data) {
+                node = node.right;
+            } else {
+                return node;
+            }
+        }
+        return null;
     }
 }
 
